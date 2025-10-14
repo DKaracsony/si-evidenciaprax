6b83b15dafeab9cd4736dbb8facae5f3fc8fd052
Peter Opál <p.opal@vizion.sk>
2025-10-14 18:51:53 +0200

    FR-01: Pripraviť súbor (key, value štuktúra) pre rôzne chybové hlášky  - BE

---
+++ resources/lang/sk/error.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'LOGIN_NEEDED' => 'Na pokračovanie sa prihláste.',
+    'SESSION_EXPIRED' => 'Relácia vypršala. Prihláste sa znova.',
+    'EMAIL_UNVERIFIED' => 'Pred pokračovaním overte svoj e-mail.',
+    'INVALID_CREDENTIALS' => 'Skontrolujte e-mail alebo heslo.',
+    'SCOPE_FORBIDDEN' => 'Tento obsah nie je dostupný pre váš účet.',
+    'PASSWORD_RESET_REQUIRED' => 'Je potrebná zmena hesla.',
+    'SERVER_ERROR' => 'Niečo sa pokazilo.'
+];
