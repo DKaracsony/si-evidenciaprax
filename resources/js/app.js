aef7e25b26d148f0acc2d62d8f005ded4b8cedbf
Peter Opál <p.opal@vizion.sk>
2025-10-30 21:55:54 +0100

    FR-03: Integrovať Vue Router a Axios pre komunikáciu s backendom - Frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import '../scss/app.scss';
+import { router } from './router'
 
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App).use(router).mount('#app');
