b121d0a7deab44956fc49e9ab6b94e7a0327c92d
Peter Opál <p.opal@vizion.sk>
2025-11-20 20:53:24 +0100

    FR-05: Vytvoriť univerzálny middleware, ktorý skontroluje oprávnenie používateľa - BackEnd

--- app/Providers/CacheServiceProvider.php
+++ app/Providers/CacheServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\Cache\CountryService;
 use App\Services\Cache\FacultyService;
 use App\Services\Cache\InternshipStatusService;
+use App\Services\Cache\PermissionService;
 use App\Services\Cache\RoleService;
 use Illuminate\Support\ServiceProvider;
 
@@ -18,6 +19,7 @@ class CacheServiceProvider extends ServiceProvider
                 $app->make(RoleService::class),
                 $app->make(CountryService::class),
                 $app->make(InternshipStatusService::class),
+                $app->make(PermissionService::class)
             ];
         });
     }
