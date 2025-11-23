ada2b6407dda75e6e1bcf701891069ee4bcc8470
Peter Opál <p.opal@vizion.sk>
2025-11-23 20:18:14 +0100

    FR-05: API endpoint – vytvoriť novú prax — BackEnd

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::middleware('auth:api')->group(function () {
     Route::patch('/account/password', [PasswordResetController::class, 'changePassword']);
     Route::get('/academic-years', [AcademicYearController::class, 'index']);
     Route::get('/student/internships', [InternshipController::class, 'index']);
-    //make a group which starts with /student
+    
     Route::prefix('student')->group(function () {
         Route::get('/internships', [InternshipController::class, 'index']);
         Route::post('/internship', [InternshipController::class, 'store'])->middleware(['permission:practice.create']);
