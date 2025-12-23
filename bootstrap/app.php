3df5971a338597f629ff995342a16192a4ec9afc
Peter Opál <p.opal@vizion.sk>
2025-12-23 20:53:16 +0100

    FR-09 – Zjednotiť chybové odpovede pre autorizáciu (401/403) – Backend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckUserPermission;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -22,5 +23,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            return response()->json([
+                'message' => __('global_error.LOGIN_NEEDED'),
+            ], 401);
+        });
     })->create();
