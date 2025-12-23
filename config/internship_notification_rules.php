b970796cfee2e88f1fefeb2c1c8544c01d770057
Attila Mancal <kismancs50@gmail.com>
2025-12-23 11:19:07 +0100

    FR-07 Doplniť logiku pre ďalšie typy zmien stavu praxe (konfiguračný súbor) - Backend

--- config/internship_notification_rules.php
+++ config/internship_notification_rules.php
@@ -20,4 +20,44 @@ return [
             'notification_text_key' => 'notification.INTERNSHIP_STATUS_CHANGED_TO_REJECTED',
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | FR-07 – Status transition rules (Garant workflow)
+    |--------------------------------------------------------------------------
+    */
+    'status_transitions' => [
+
+        'Vytvorená' => [
+            'Potvrdená' => [
+                'requires_explanation' => false,
+                'send_email' => false,
+            ],
+            'Zamietnutá' => [
+                'requires_explanation' => true,
+                'send_email' => false,
+            ],
+        ],
+
+        'Potvrdená' => [
+            'Schválená' => [
+                'requires_explanation' => false,
+                'send_email' => true,
+                'email_type' => 'confirmed_to_approved',
+            ],
+        ],
+
+        'Schválená' => [
+            'Obhájená' => [
+                'requires_explanation' => false,
+                'send_email' => true,
+                'email_type' => 'approved_to_defended',
+            ],
+            'Neobhájená' => [
+                'requires_explanation' => true,
+                'send_email' => true,
+                'email_type' => 'approved_to_not_defended',
+            ],
+        ],
+    ],
 ];
