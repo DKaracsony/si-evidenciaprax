d3bf1f918fcf749afba91f637e31eaf06da1cf0f
AttilaMAncal <kismancs50@gmail.com>
2026-01-04 12:49:48 +0100

    FR-10 Generovanie CSV súboru z dát praxí + FR-10 Formát dátumov a textov v CSV – Backend

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\InternshipQueryBuilder;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ExportController extends Controller
 {
@@ -16,6 +17,56 @@ class ExportController extends Controller
             return response()->json(['message' => __('internship.NO_DATA_TO_EXPORT')], 404);
         }
 
-        return null; //TODO: Atus - dokoncit export do csv, filtre su hotove, netreba to ulozit do suboru, staci vratit ako stiahnuty subor
+        $fileName = 'internships_' . now()->format('Y-m-d_H-i-s') . '.csv';
+
+        return response()->streamDownload(function () use ($internships) {
+            $out = fopen('php://output', 'w');
+
+            fwrite($out, "\xEF\xBB\xBF");
+
+            fputcsv($out, [
+                'id',
+                'student_profile_id',
+                'company_id',
+                'academic_year_id',
+                'start_date',
+                'date_to',
+                'is_draft',
+                'submitted_at',
+                'created_at',
+                'updated_at',
+            ]);
+
+            foreach ($internships as $i) {
+                fputcsv($out, [
+                    $i->id,
+                    $i->student_profile_id,
+                    $i->company_id,
+                    $i->academic_year_id,
+                    $this->fmtDate($i->start_date),
+                    $this->fmtDate($i->date_to),
+                    (int) $i->is_draft,
+                    $this->fmtDateTime($i->submitted_at),
+                    $this->fmtDateTime($i->created_at),
+                    $this->fmtDateTime($i->updated_at),
+                ]);
+            }
+
+            fclose($out);
+        }, $fileName, [
+            'Content-Type' => 'text/csv; charset=UTF-8',
+        ]);
+    }
+
+    private function fmtDate($value): ?string
+    {
+        if (empty($value)) return null;
+        return \Carbon\Carbon::parse($value)->format('Y-m-d');
+    }
+
+    private function fmtDateTime($value): ?string
+    {
+        if (empty($value)) return null;
+        return \Carbon\Carbon::parse($value)->format('Y-m-d H:i:s');
     }
 }
